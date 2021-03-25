2de176624228664ca7eeaa762b21ac0a010a0c1a
Ali Bahadoripoor <ali.bahadori@mail.de>
2021-03-25 04:23:32 -0700

    Initial Commit

---
+++ iOS-Party/iOS-Party/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Party
+//
+//  Created by Ali Bahadori on 25.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
